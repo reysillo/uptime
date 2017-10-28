9ec8b1aecae704767c3d8f1402eef024c3eba4d9
Reinaldo Calderón <rcalderon@easysol.net>
2017-10-27 19:58:09 -0500

    Updated nodemailer version to 4.0.0 & fixed mongodb login when no credentials

--- app.js
+++ app.js
@@ -29,48 +29,48 @@ var app = module.exports = express();
 var server = http.createServer(app);
 
 app.configure(function(){
-  app.use(app.router);
-  // the following middlewares are only necessary for the mounted 'dashboard' app, 
-  // but express needs it on the parent app (?) and it therefore pollutes the api
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('Z5V45V6B5U56B7J5N67J5VTH345GC4G5V4'));
-  app.use(express.cookieSession({
-    key:    'uptime',
-    secret: 'FZ5HEE5YHD3E566756234C45BY4DSFZ4',
-    proxy:  true,
-    cookie: { maxAge: 60 * 60 * 1000 }
-  }));
-  app.set('pollerCollection', new PollerCollection());
+    app.use(app.router);
+    // the following middlewares are only necessary for the mounted 'dashboard' app,
+    // but express needs it on the parent app (?) and it therefore pollutes the api
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('Z5V45V6B5U56B7J5N67J5VTH345GC4G5V4'));
+    app.use(express.cookieSession({
+        key:    'uptime',
+        secret: 'FZ5HEE5YHD3E566756234C45BY4DSFZ4',
+        proxy:  true,
+        cookie: { maxAge: 60 * 60 * 1000 }
+    }));
+    app.set('pollerCollection', new PollerCollection());
 });
 
 // load plugins (may add their own routes and middlewares)
 config.plugins.forEach(function(pluginName) {
-  var plugin = require(pluginName);
-  if (typeof plugin.initWebApp !== 'function') return;
-  console.log('loading plugin %s on app', pluginName);
-  plugin.initWebApp({
-    app:       app,
-    api:       apiApp,       // mounted into app, but required for events
-    dashboard: dashboardApp, // mounted into app, but required for events
-    io:        io,
-    config:    config,
-    mongoose:  mongoose
-  });
+    var plugin = require(pluginName);
+    if (typeof plugin.initWebApp !== 'function') return;
+    console.log('loading plugin %s on app', pluginName);
+    plugin.initWebApp({
+        app:       app,
+        api:       apiApp,       // mounted into app, but required for events
+        dashboard: dashboardApp, // mounted into app, but required for events
+        io:        io,
+        config:    config,
+        mongoose:  mongoose
+    });
 });
 
 app.emit('beforeFirstRoute', app, apiApp);
 
 app.configure('development', function() {
-  if (config.verbose) mongoose.set('debug', true);
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    if (config.verbose) mongoose.set('debug', true);
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function() {
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
+    var oneYear = 31557600000;
+    app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+    app.use(express.errorHandler());
 });
 
 // Routes
@@ -80,11 +80,11 @@ app.use('/api', apiApp);
 app.emit('beforeDashboardRoutes', app, dashboardApp);
 app.use('/dashboard', dashboardApp);
 app.get('/', function(req, res) {
-  res.redirect('/dashboard/events');
+    res.redirect('/dashboard/events');
 });
 
 app.get('/favicon.ico', function(req, res) {
-  res.redirect(301, '/dashboard/favicon.ico');
+    res.redirect(301, '/dashboard/favicon.ico');
 });
 
 app.emit('afterLastRoute', app);
@@ -93,47 +93,47 @@ app.emit('afterLastRoute', app);
 var io = socketIo.listen(server);
 
 io.configure('production', function() {
-  io.enable('browser client etag');
-  io.set('log level', 1);
+    io.enable('browser client etag');
+    io.set('log level', 1);
 });
 
 io.configure('development', function() {
-  if (!config.verbose) io.set('log level', 1);
+    if (!config.verbose) io.set('log level', 1);
 });
 
 CheckEvent.on('afterInsert', function(event) {
-  io.sockets.emit('CheckEvent', event.toJSON());
+    io.sockets.emit('CheckEvent', event.toJSON());
 });
 
 io.sockets.on('connection', function(socket) {
-  socket.on('set check', function(check) {
-    socket.set('check', check);
-  });
-  Ping.on('afterInsert', function(ping) {
-    socket.get('check', function(err, check) {
-      if (ping.check == check) {
-        socket.emit('ping', ping);
-      }
+    socket.on('set check', function(check) {
+        socket.set('check', check);
     });
-  });
+    Ping.on('afterInsert', function(ping) {
+        socket.get('check', function(err, check) {
+            if (ping.check == check) {
+                socket.emit('ping', ping);
+            }
+        });
+    });
 });
 
 // old way to load plugins, kept for BC
 fs.exists('./plugins/index.js', function(exists) {
-  if (exists) {
-    var pluginIndex = require('./plugins');
-    var initFunction = pluginIndex.init || pluginIndex.initWebApp;
-    if (typeof initFunction === 'function') {
-      initFunction({
-        app:       app,
-        api:       apiApp,       // mounted into app, but required for events
-        dashboard: dashboardApp, // mounted into app, but required for events
-        io:        io,
-        config:    config,
-        mongoose:  mongoose
-      });
+    if (exists) {
+        var pluginIndex = require('./plugins');
+        var initFunction = pluginIndex.init || pluginIndex.initWebApp;
+        if (typeof initFunction === 'function') {
+            initFunction({
+                app:       app,
+                api:       apiApp,       // mounted into app, but required for events
+                dashboard: dashboardApp, // mounted into app, but required for events
+                io:        io,
+                config:    config,
+                mongoose:  mongoose
+            });
+        }
     }
-  }
 });
 
 module.exports = app;
@@ -141,31 +141,31 @@ module.exports = app;
 var monitorInstance;
 
 if (!module.parent) {
-  var serverUrl = url.parse(config.url);
-  var port;
-  if (config.server && config.server.port) {
-    console.error('Warning: The server port setting is deprecated, please use the url setting instead');
-    port = config.server.port;
-  } else {
-    port = serverUrl.port;
-    if (port === null) {
-      port = 80;
+    var serverUrl = url.parse(config.url);
+    var port;
+    if (config.server && config.server.port) {
+        console.error('Warning: The server port setting is deprecated, please use the url setting instead');
+        port = config.server.port;
+    } else {
+        port = serverUrl.port;
+        if (port === null) {
+            port = 80;
+        }
     }
-  }
-  var port = process.env.PORT || port;
-  var host = process.env.HOST || serverUrl.hostname;
-  server.listen(port, function(){
-    console.log("Express server listening on host %s, port %d in %s mode", host, port, app.settings.env);
-  });
-  server.on('error', function(e) {
-    if (monitorInstance) {
-      monitorInstance.stop();
-      process.exit(1);
-    }
-  });
+    var port = process.env.PORT || port;
+    var host = process.env.HOST || serverUrl.hostname;
+    server.listen(port, function(){
+        console.log("Express server listening on host %s, port %d in %s mode", host, port, app.settings.env);
+    });
+    server.on('error', function(e) {
+        if (monitorInstance) {
+            monitorInstance.stop();
+            process.exit(1);
+        }
+    });
 }
 
 // monitor
 if (config.autoStartMonitor) {
-  monitorInstance = require('./monitor');
+    monitorInstance = require('./monitor');
 }
--- plugins/email/index.js
+++ plugins/email/index.js
@@ -5,7 +5,7 @@
  *
  * Installation
  * ------------
- * This plugin is disabled by default. To enable it, add its entry 
+ * This plugin is disabled by default. To enable it, add its entry
  * to the `plugins` key of the configuration:
  *
  *   // in config/production.yaml
@@ -14,10 +14,10 @@
  *
  * Usage
  * -----
- * This plugin sends an email each time a check is started, goes down, or goes back up. 
+ * This plugin sends an email each time a check is started, goes down, or goes back up.
  * When the check goes down, the email contains the error details:
  *
- *   Object: [Down] Check "FooBar" just went down
+ *   Object: [Down] Check "FooBar" just went down
  *   On Thursday, September 4th 1986 8:30 PM,
  *   a test on URL "http://foobar.com" failed with the following error:
  *
@@ -36,7 +36,7 @@
  *     method:      SMTP  # possible methods are SMTP, SES, or Sendmail
  *     transport:         # see https://github.com/andris9/nodemailer for transport options
  *       service:   Gmail
- *       auth:            
+ *       auth:
  *         user:    foobar@gmail.com
  *         pass:    gursikso
  *     event:
@@ -44,7 +44,7 @@
  *       down:      true
  *       paused:    false
  *       restarted: false
- *     message:           
+ *     message:
  *       from:     'Fred Foo <foo@blurdybloop.com>'
  *       to:       'bar@blurdybloop.com, baz@blurdybloop.com'
  *     # The email plugin also uses the main `url` param for hyperlinks in the sent emails
@@ -54,35 +54,55 @@ var nodemailer = require('nodemailer');
 var moment     = require('moment');
 var CheckEvent = require('../../models/checkEvent');
 var ejs        = require('ejs');
+var bunyan = require('bunyan');
 
 exports.initWebApp = function(options) {
-  var config = options.config.email;
-  var mailer = nodemailer.createTransport(config.method, config.transport);
-  var templateDir = __dirname + '/views/';
-  CheckEvent.on('afterInsert', function(checkEvent) {
-    if (!config.event[checkEvent.message]) return;
-    checkEvent.findCheck(function(err, check) {
-      if (err) return console.error(err);
-      var filename = templateDir + checkEvent.message + '.ejs';
-      var renderOptions = {
-        check: check,
-        checkEvent: checkEvent,
-        url: options.config.url,
-        moment: moment,
-        filename: filename
-      };
-      var lines = ejs.render(fs.readFileSync(filename, 'utf8'), renderOptions).split('\n');
-      var mailOptions = {
-        from:    config.message.from,
-        to:      config.message.to,
-        subject: lines.shift(),
-        text:    lines.join('\n')
-      };
-      mailer.sendMail(mailOptions, function(err2, response) {
-        if (err2) return console.error('Email plugin error: %s', err2);
-        console.log('Notified event by email: Check ' + check.name + ' ' + checkEvent.message);
-      });
+    var config = options.config.email;
+    var transporter = nodemailer.createTransport({
+        host: config.transport.host,
+        port: config.transport.port,
+        secure: config.transport.secure,
+        auth: {
+            user: config.transport.auth.user,
+            pass: config.transport.auth.pass
+        },
+        logger: bunyan.createLogger({
+            name: 'nodemailer'
+        }),
+        debug: true
+    }, {
+        // sender info
+        from: config.message.from
     });
-  });
-  console.log('Enabled Email notifications');
+    var templateDir = __dirname + '/views/';
+    CheckEvent.on('afterInsert', function(checkEvent) {
+        if (!config.event[checkEvent.message]) return;
+        checkEvent.findCheck(function(err, check) {
+            if (err) return console.error(err);
+            var filename = templateDir + checkEvent.message + '.ejs';
+            console.log("filename : " + filename)
+            var renderOptions = {
+                check: check,
+                checkEvent: checkEvent,
+                url: options.config.url,
+                moment: moment,
+                filename: filename
+            };
+            var lines = ejs.render(fs.readFileSync(filename, 'utf8'), renderOptions).split('\n');
+            mailist = config.message.to.split(",")
+            let message = {
+                to: mailist,
+                subject: lines.shift(),
+                text: lines.join('\n')
+            } ;
+            console.log('Sending Mail');
+            transporter.sendMail(message, (error, info) => {
+                if (error) return console.error('Email plugin error: %s', error);
+            console.log('Message sent successfully!');
+            console.log('Server responded with "%s"', info.response);
+            transporter.close();
+        });
+        });
+    });
+    console.log('Enabled Email notifications');
 };
